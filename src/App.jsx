dc1277dfcde9df3b781593f7cb839ed27a3055f0
MayddaR <mayddarodriguez@gmail.com>
2023-05-31 20:28:40 -0500

    Parcial

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
+import {useState} from 'react';
+import './App.css';
+import {Form} from "./componentes/Form";
+import {Card} from './componentes/Card';
+
 function App() {
   //Aqui deberias agregar los estados y los handlers para los inputs
+  const [animalesPreferidos, setAnimalesPreferidos] = useState([])
+
+
+  function agregarAnimal(animal, nombre) {
+  
+    const nuevoAnimal = {
+      animal,
+      nombre,
+    }
+    setAnimalesPreferidos([...animalesPreferidos, nuevoAnimal])
+  }
 
   return (
     <div className="App">
-      <h1>Elige un color</h1>
-      <form>{/* aqui deberias escribir tu codigo */}</form>
+      <h1>Elige un animal</h1>
+      <Form agregarAnimal={agregarAnimal} 
+        />
+      <div>
+      {animalesPreferidos.map((animalPreferido) => {
+          return (
+            <Card
+              key={animalPreferido.id}
+              animal={animalPreferido.animal}
+              nombre={animalPreferido.nombre}
+              />
+          )
+        })}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
